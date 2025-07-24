389ddcbc78cf097b6c14d387046b207b31270fc4
SyazWak <115389125+SyazWak@users.noreply.github.com>
2025-07-24 15:51:31 +0800

    Add function to make web stay active

--- public/script.js
+++ public/script.js
@@ -97,3 +97,18 @@ document.addEventListener('keydown', function(event) {
         refreshData();
     }
 });
+
+const url = `https://mulah-simple-task-assesment.onrender.com`; 
+const interval = 30000; 
+
+function reloadWebsite() {
+  axios.get(url)
+    .then(response => {
+      console.log(`Reloaded at ${new Date().toISOString()}: Status Code ${response.status}`);
+    })
+    .catch(error => {
+      console.error(`Error reloading at ${new Date().toISOString()}:`, error.message);
+    });
+}
+
+setInterval(reloadWebsite, interval);
